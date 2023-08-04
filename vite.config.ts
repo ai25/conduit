fbb2d1b90ca01dbc1a1eafb51dd60365446b3795
vr10t <96934297+vr10t@users.noreply.github.com>
2023-08-04 17:05:27 +0100

    vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
 import solid from "solid-start/vite";
 import { defineConfig } from "vite";
-import devtools from 'solid-devtools/vite'
+import devtools from "solid-devtools/vite";
+import vercel from "solid-start-vercel";
 
 export default defineConfig({
-  plugins: [devtools({
+  plugins: [
+    devtools({
       /* features options - all disabled by default */
       autoname: true, // e.g. enable autoname
       locator: true, // enables DOM locator tab
     }),
-    solid()],
+    solid({ adapter: vercel({ edge: true }) }),
+  ],
 });
