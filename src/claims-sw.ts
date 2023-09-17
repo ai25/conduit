c40bb17c079efea7b776b57f6d9733ce57062f71
vr10t <96934297+vr10t@users.noreply.github.com>
2023-09-17 13:02:29 +0100

    4

--- src/claims-sw.ts
+++ src/claims-sw.ts
@@ -16,7 +16,7 @@ precacheAndRoute(self.__WB_MANIFEST);
 cleanupOutdatedCaches();
 
 let allowlist: undefined | RegExp[];
-if (import.meta.env.DEV) allowlist = [/.*/];
+allowlist = [/.*/];
 
 // to allow work offline
 registerRoute(new NavigationRoute(createHandlerBoundToURL("/"), { allowlist }));
