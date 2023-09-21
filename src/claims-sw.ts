cf4204180ba7ecb2b6939e3953dc76a432f3c826
vr10t <96934297+vr10t@users.noreply.github.com>
2023-09-21 11:26:04 +0100

    fix wb

--- src/claims-sw.ts
+++ src/claims-sw.ts
@@ -10,7 +10,7 @@ import { StaleWhileRevalidate } from "workbox-strategies";
 declare let self: ServiceWorkerGlobalScope;
 
 // self.__WB_MANIFEST is default injection point
-precacheAndRoute(self.__WB_MANIFEST);
+// precacheAndRoute(self.__WB_MANIFEST);
 
 // clean old assets
 cleanupOutdatedCaches();
