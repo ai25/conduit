b52617ceb8b55c10b0ef56c679446840982ec0bd
vr10t <96934297+vr10t@users.noreply.github.com>
2024-07-14 14:25:27 +0100

    fix #7

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -18,7 +18,7 @@ export default createHandler(() => {
               name="viewport"
               content="width=device-width, initial-scale=1"
             />
-            <link rel="icon" href="/favicon.ico" />
+            <link rel="icon" href="/favicon.png" />
             {assets}
           </head>
           <body class="bg-black text-white scrollbar">
